8c2ffecd25522e767696c195cc0ef59671ba320e
Antonio Entringer <61886028+AntonioEntringer@users.noreply.github.com>
2023-07-11 09:42:40 -0300

    Update cliente-http.py

--- cliente/cliente-http.py
+++ cliente/cliente-http.py
@@ -1,4 +1,11 @@
 import requests
+import os
+
+def listar_arquivos(pasta):
+    arquivos = os.listdir(pasta)
+    for i, arquivo in enumerate(arquivos, start=1):
+        print(f"{i}. {arquivo}")
+    return arquivos
 
 def enviar_arquivo(nome_arquivo, endereco_servidor, porta_servidor):
     url = f"http://{endereco_servidor}:{porta_servidor}/upload"
@@ -12,10 +19,20 @@ def enviar_arquivo(nome_arquivo, endereco_servidor, porta_servidor):
         print("Ocorreu um erro ao enviar o arquivo.")
 
 if __name__ == "__main__":
-    nome_arquivo = input("Digite o nome do arquivo que deseja enviar: ")
-    endereco_servidor = input("Digite o endereço IP do servidor: ")
-    print('Press CTRL+C to quit')
-    porta_servidor = 5001
+    pasta = '/root/enviar'
+    arquivos = listar_arquivos(pasta)
 
-    enviar_arquivo(nome_arquivo, endereco_servidor, porta_servidor)
+    while True:
+        numero_arquivo = input("Digite o número do arquivo que deseja enviar (ou 'q' para sair): ")
+        if numero_arquivo == 'q':
+            break
 
+        try:
+            indice = int(numero_arquivo) - 1
+            nome_arquivo = arquivos[indice]
+            endereco_servidor = input("Digite o endereço IP do servidor: ")
+            porta_servidor = 5001
+
+            enviar_arquivo(os.path.join(pasta, nome_arquivo), endereco_servidor, porta_servidor)
+        except (ValueError, IndexError):
+            print("Número inválido. Tente novamente.")
